374bd03fd836cd756286fd9425825310834a3171
Ana Maria Benedetti <ambenedetti@gmail.com>
2019-11-06 15:49:35 +0100

    finished yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants, except: [:update, :destroy, :edit] do
+    resources :reviews, except: [:update, :destroy, :edit]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{ name: 'Haru',
+category: 'japanese',
+address: 'lower east side',
+phone_number:'9175555555' },
+
+{ name: 'Lo Mein',
+category: 'chinese',
+address: 'chinatown',
+phone_number:'9176666666' },
+
+{ name: 'Entrecote',
+category: 'french',
+address: 'uppper east side',
+phone_number:'9177777777' },
+
+{ name: 'Antwerp',
+category: 'belgian',
+address: 'bushwick',
+phone_number:'9178888888' },
+
+{ name: 'Vezzo',
+category: 'italian',
+address: 'gramercy',
+phone_number:'9179999999' }])
+
+
